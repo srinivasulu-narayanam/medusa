5af0eeea7428dfa316c904efec3406ca988331d9
srinivasulu.n <narayanam.srinivasulu@increscotech.com>
2024-10-24 14:16:06 +0530

    feat(payment): add razorpay

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,6 +1,6 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+import { loadEnv, defineConfig } from "@medusajs/framework/utils";
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
 module.exports = defineConfig({
   projectConfig: {
@@ -11,15 +11,33 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+    },
   },
   modules: [
     {
       resolve: "./src/modules/marketplace",
     },
     // {
-    //   resolve: ""
-    // }
+    //   resolve: "@medusajs/medusa/payment",
+    //   options: {
+    //     providers: [
+    //       {
+    //         resolve: "./src/modules/my-payment",
+    //         id: "razorpay",
+    //         options: {
+    //           key_id: process.env.RAZORPAY_ID,
+    //           key_secret: process.env.RAZORPAY_SECRET,
+    //           razorpay_account: process.env.RAZORPAY_ACCOUNT,
+    //           automatic_expiry_period: 30 /*any value between 12 minutes and 30 days expressed in minutes*/,
+    //           manual_expiry_period: 7200,
+    //           refund_speed: "normal",
+    //           // webhook_secret: process.env.RAZORPAY_WEBHOOK_SECRET,
+    //           auto_capture: true, // if you want to automatically capture,
+    //         },
+    //       },
+    //     ],
+    //   },
+    // },
   ],
   // plugins:[
   //   {
@@ -36,4 +54,4 @@ module.exports = defineConfig({
   //     },
   //   },
   // ]
-})
+});
--- src/modules/my-payment/index.ts
+++ src/modules/my-payment/index.ts
@@ -1,4 +1,4 @@
-import RazorpayProviderService from "./service"
+import RazorpayProviderService from "./razorpay"
 import { 
   ModuleProvider, 
   Modules
